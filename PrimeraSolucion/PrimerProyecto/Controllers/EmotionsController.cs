59fe2f0e8e072978a93796b14dc57384c90734d2
Joseph Daniel Rojas Flores <a2019113821@estudiantes.upsa.edu.bo>
2021-02-20 11:40:56 -0400

    Emocion sin modelo

---
+++ PrimeraSolucion/PrimerProyecto/Controllers/EmotionsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmotionsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ConNumero")]
+        public string Emocion(int suerte)
+        {
+            if (suerte == 0)
+                return "sos una persona indiferente";
+            if (suerte == 1)
+                return "sos una persona feliz";
+            if (suerte == 3)
+                return "sos una persona aburrida";
+            return "estoy muy ocupado, no puedo atenderte";
+        }
+        [HttpPost]
+        [Route("ConNumero")]
+        public string Emocion2([FromHeader] int suerte)
+        {
+            if (suerte == 0)
+                return "sos una persona indiferente";
+            if (suerte == 1)
+                return "sos una persona feliz";
+            if (suerte == 3)
+                return "sos una persona aburrida";
+            return "estoy muy ocupado, no puedo atenderte";
+        }
+    }
+}
